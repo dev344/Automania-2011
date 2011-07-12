33858e941e2d5de7c74044bc9c909404943557b1
Y Devesh <dev344@gmail.com>
2011-07-12 17:36:22 +0530

    First commit

---
+++ src/org/shaastra/automania/MainGameServer.java
@@ -0,0 +1,14 @@
+package org.shaastra.automania;
+
+public class MainGameServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Eclipse!");
+
+	}
+
+}
